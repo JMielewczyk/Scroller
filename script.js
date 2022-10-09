546f30214ef1536671a4a1b2d50e848462a7eeff
Jakub <jkbmielewczyk@gmail.com>
2022-10-09 15:19:16 +0200

    Added navigation.

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 const sections = document.querySelectorAll('.section');
 const main = document.querySelector('.main');
+const sectionSelector = document.querySelector('.section-selector');
+let allDots;
 
 let currentSection = 0;
-
 let isThrottled = false;
+
+const addDots = () => {
+    for (section of sections) {
+        const element = document.createElement('div');
+        element.classList.add('dot')
+        sectionSelector.appendChild(element)
+    }
+    allDots = [...document.querySelectorAll('.dot')]
+}
+addDots()
+
+
+const chooseSection = (e) => {
+    if (e.target.classList.value === 'dot') {
+        const index = allDots.indexOf(e.target)
+        currentSection = index;
+        scrollInto(currentSection)
+    }
+}
+sectionSelector.addEventListener('click', chooseSection)
 
 window.addEventListener('wheel', (e) => {
     if (isThrottled) return
@@ -11,7 +32,7 @@ window.addEventListener('wheel', (e) => {
 
     setTimeout(() => {
         isThrottled = false
-    }, 500)
+    }, 300)
 
     const direction = e.deltaY > 0 ? 1 : -1;
     if (direction === 1) {
@@ -21,8 +42,15 @@ window.addEventListener('wheel', (e) => {
         if (currentSection === 0) return
         currentSection--
     }
+    scrollInto(currentSection)
+})
+
+function scrollInto(value) {
     sections[currentSection].scrollIntoView({
         behavior: 'smooth',
         block: "start",
     })
-})
+    allDots.forEach(dot => dot.classList.remove('active'))
+    allDots[currentSection].classList.add('active')
+}
+scrollInto(currentSection)
